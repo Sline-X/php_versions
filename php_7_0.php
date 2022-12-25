040c136d637a5cd658d29d744be927dc873927b5
“Sline-X” <kipa.maksim99@gmail.com>
2022-12-26 01:07:47 +0300

    php 7.0

--- php_7_0.php
+++ php_7_0.php
@@ -58,6 +58,11 @@ echo "\u{0073}";
 //десериализация с фильтрацией, даёт более высокий уровень безопасности 
 //unserialize
 
+//Эта функциональность обеспечивает более высокий уровень безопасности при десериализации объектов 
+//с непроверенными данными. Это позволяет предотвратить возможную инъекцию кода, позволяя 
+//разработчику использовать белый список классов для десериализации.
+
+
 // Преобразование всех объектов в __PHP_Incomplete_Class
 $data = unserialize($foo, ["allowed_classes" => false]);
 
@@ -66,3 +71,53 @@ $data = unserialize($foo, ["allowed_classes" => ["MyClass", "MyClass2"]]);
 
 // Поведение по умолчанию принимает все классы (можно просто не задавать второй аргумент)
 $data = unserialize($foo, ["allowed_classes" => true]);
+
+
+// ожидания assert, если перывй аргумент false, то можно получить исключение
+//используется для тестов, например phpunit
+ini_set('assert.exception', 1);
+
+class CustomError extends AssertionError {}
+
+assert(false, new CustomError('Сообщение об ошибке'));
+
+
+//групповые объявления use, можно группировать из одного неймспейса
+use some\namespace\{ClassA, ClassB, ClassC as C};
+use function some\namespace\{fn_a, fn_b, fn_c};
+use const some\namespace\{ConstA, ConstB, ConstC};
+
+//было
+use some\namespace\ClassA;
+use some\namespace\ClassB;
+use some\namespace\ClassC as C;
+
+use function some\namespace\fn_a;
+use function some\namespace\fn_b;
+use function some\namespace\fn_c;
+
+use const some\namespace\ConstA;
+use const some\namespace\ConstB;
+use const some\namespace\ConstC;
+
+//Добавлена возможность доступа к методам и свойствам класса при клонировании,
+// то есть (clone $foo)->bar().
+
+
+
+//Deprecated
+//конструкторы PHP4
+class foo {
+    function foo() {
+        echo 'Я конструктор!';
+    }
+}
+
+//Статические вызовы нестатических методов
+class foo {
+    function bar() {
+        echo 'Я не статический!';
+    }
+}
+
+foo::bar();
