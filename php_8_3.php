4e6437f80fbd7f07df21a7ab01fa5b74b5b0600b
Maksim Kipa <m.kipa@webpractik.ru>
2024-01-18 15:25:15 +0300

    add php8.3 Dynamic class constant fetch

--- php_8_3.php
+++ php_8_3.php
@@ -21,6 +21,26 @@ interface I {
 class Foo implements I {
     const string PHP = [];
 }
-
 // Fatal error: Cannot use array as value for class constant
 // Foo::PHP of type string
+
+
+//Динамическое получение констант класса
+
+//было
+class Foo {
+    const PHP = 'PHP 8.2';
+}
+
+$searchableConstant = 'PHP';
+
+var_dump(constant(FOO::class . "::{$searchableConstant}"));
+
+//стало
+class Foo {
+    const PHP = 'PHP 8.3';
+}
+
+$searchableConstant = 'PHP';
+
+var_dump(Foo::{$searchableConstant});
